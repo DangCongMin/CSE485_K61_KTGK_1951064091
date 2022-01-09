b3f56f26dac8820261d41adfeb7d1ba36fae6d99
DangCongMin <96401808+DangCongMin@users.noreply.github.com>
2022-01-09 11:59:04 +0700

    Minh

--- index.php
+++ index.php
@@ -29,6 +29,39 @@
 
     $object->$action();
 
+    Private Sub Command1_Click()
+If (Text1.Text <> "" And Text2.Text <> "") Then
+Combo1.AddItem Text1.Text
+List1.AddItem Text2.Text
+Text1.Text = ""
+Text2.Text = ""
+Else
+MsgBox "Ban hay dien day du thong tin", vbOKOnly + vbCritical, "Thong bao"
+End If
+End Sub
+
+
+Private Sub Command2_Click()
+Dim n As Integer
+Dim m As Integer
+n = Combo1.ListIndex
+m = List1.ListIndex
+Combo1.RemoveItem n
+List1.RemoveItem m
+Combo1.AddItem Text1.Text, n
+List1.AddItem Text2.Text, m
+Combo1.ListIndex = n
+List1.ListIndex = m
+End Sub
+
+
+Private Sub Command3_Click()
+Combo1.RemoveItem Combo1.ListIndex
+List1.RemoveItem List1.ListIndex
+Text1.Text = ""
+Text2.Text = ""
+Command3.Enabled = False
+End Sub
 
 
 ?>
