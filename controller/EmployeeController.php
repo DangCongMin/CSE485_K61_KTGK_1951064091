3a8f8d938c9cefc0f42070f464ebd2c33f0723fd
DangCongMin <96401808+DangCongMin@users.noreply.github.com>
2022-01-09 11:31:51 +0700

    Minh

---
+++ controller/EmployeeController.php
@@ -0,0 +1,24 @@
+<?php
+    require_once 'model/EmployeeModel.php';
+    class EmployeeController{
+      
+        function index(){
+           
+            $empModel = new EmployeeModel();
+            $emps = $empModel->getAllEmps();
+           
+            require_once 'view/employee/index.php';
+        }
+
+        function add(){
+            
+        }
+
+        function edit(){
+            
+        }
+    }
+
+
+
+?>
